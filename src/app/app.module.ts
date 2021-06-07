8a40a26cdccac4e2cc1523d3c5fe08efbe6ae476
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2021-06-07 15:03:01 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NgxEditorModule } from 'ngx-editor';
+import {NgxPrintModule} from 'ngx-print';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule  NgxEditorModule,NgxPrintModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
