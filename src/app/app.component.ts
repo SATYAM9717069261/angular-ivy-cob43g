564a02c2643e939d60bc0af0854178e48d7bda9f
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2021-06-07 14:57:09 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, VERSION } from '@angular/core';
-
+import { Editor } from 'ngx-editor';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  constructor() { }
+  editor:any;
+  ngOnInit(): void { this.editor = new Editor();  }
+
 }
